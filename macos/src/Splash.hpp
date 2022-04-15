30809d5db556eb6cfc2e72f82af3b8b43f8e3828
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-15 17:54:36 +0200

    ST-BHJ project changes

---
+++ macos/src/Splash.hpp
@@ -0,0 +1,26 @@
+// 
+// SchumakerTeam
+// Hudson Schumaker
+//
+
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+class Splash final {
+    
+    private:
+        SDL_Renderer* renderer;
+        SDL_Texture* logoTexture; 
+        
+        void load();  
+        void loop();
+        void render();
+        void input();
+        void unload();  
+    
+    public:
+        Splash(SDL_Renderer *renderer);
+        ~Splash();
+};
