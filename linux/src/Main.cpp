f630ace81793849f37d165ae19903fb4ff8d8d3a
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-16 16:25:15 +0200

    ST-BHJ linux OpenGL window

--- linux/src/Main.cpp
+++ linux/src/Main.cpp
@@ -18,7 +18,7 @@ void setUp() {
         Common::TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         Common::WIDTH,
         Common::HEIGHT,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_METAL 
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL 
     );
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);   
